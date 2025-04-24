3f7d51830a8f7ac28496a378945a755f252c94f1
KRITHIKR007 <KRITHIK.R007@outlook.com>
2025-04-24 22:53:43 +0530

    deleted: 1,1

--- vercel_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is a simplified build script specifically for Vercel
-
-echo "Installing dependencies..."
-pip install -r requirements-vercel.txt
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
